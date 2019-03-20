d6d6dc3b6b0d309ba55d155c22ea8b4a7903e88d
Michael Lamb <503lamb@gmail.com>
2019-03-20 10:22:31 -0600

    fix bug

--- index.js
+++ index.js
@@ -458,9 +458,9 @@ class SortableListView extends React.Component {
   }
 
   componentDidMount() {
-    InteractionManager.runAfterInteractions(() => {
-      this.timer = setTimeout(() => this && this.measureWrapper(), 0)
-    })
+    // InteractionManager.runAfterInteractions(() => {
+    //   this.timer = setTimeout(() => this && this.measureWrapper(), 0)
+    // })
   }
 
   componentWillReceiveProps(props) {
